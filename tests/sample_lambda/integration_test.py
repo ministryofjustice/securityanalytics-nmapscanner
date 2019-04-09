a93fb15aaabb20c3c53d95f428b507fbc4fc37c3
Matthew Amos <mamos@scottlogic.com>
2019-04-09 16:45:08 +0100

    Make environments for devs and build server both work

--- tests/sample_lambda/integration_test.py
+++ tests/sample_lambda/integration_test.py
@@ -5,14 +5,22 @@ import requests
 import json
 from requests_aws4auth import AWS4Auth
 
+
+# TODO move this to shared layer
+def get_auth():
+    session = boto3.Session() \
+        if 'AWS_ACCESS_KEY_ID' in os.environ \
+        else boto3.Session(profile_name='sec-an')
+    credentials = session.get_credentials()
+    return AWS4Auth(
+        credentials.access_key,
+        credentials.secret_key,
+        region,
+        "execute-api",
+        session_token=credentials.token)
+
+
 region = os.environ['AWS_REGION']
-credentials = boto3.Session(profile_name='sec-an').get_credentials()
-aws_auth = AWS4Auth(
-    credentials.access_key,
-    credentials.secret_key,
-    region,
-    "execute-api",
-    session_token=credentials.token)
 
 
 # TODO make this into a reusable util
@@ -23,6 +31,6 @@ def get_url(endpoint, method):
 @pytest.mark.integration
 def test_integration():
     url = get_url('sample', 'GET')
-    response = requests.get(url, auth=aws_auth)
+    response = requests.get(url, auth=get_auth())
     assert response.status_code == 200
     assert "hello lambda world" in response.text
