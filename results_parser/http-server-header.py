12a8fddad77e27a1095ce7eb0076671ee1970ab9
Matthew Amos <mamos@scottlogic.com>
2019-04-29 18:08:18 +0100

    Added in plugins for scripts, plugins for http-server-header and for ssl-enum-ciphers, created sample index and query

---
+++ results_parser/http-server-header.py
@@ -0,0 +1,2 @@
+def process_script(script):
+    return {"http-server-header": script.elem.cdata}
--- results_parser/results_parser.py
+++ results_parser/results_parser.py
@@ -11,6 +11,7 @@ import untangle
 import datetime
 import pytz
 from urllib.parse import unquote_plus
+import importlib.util
 
 
 region = os.environ["REGION"]
@@ -48,23 +49,40 @@ def process_results(topic, bucket, key):
 def process_host_results(topic, host):
     address = host.address["addr"]
     address_type = host.address["addrtype"]
-    parsed_time = datetime.datetime.fromtimestamp(int(host["endtime"]), pytz.utc)
-    scan_time = parsed_time.isoformat().replace('+00:00', 'Z')
-    print(f"Looking at host: {(address, address_type)} scanned at {scan_time}")
+    start_time, end_time = map(
+        lambda f:
+            datetime.datetime.fromtimestamp(int(host[f]), pytz.utc).isoformat().replace('+00:00', 'Z'),
+        ("starttime", "endtime"))
+    print(f"Looking at host: {(address, address_type)} scanned at {end_time}")
 
     host_names = []
+    os_info = []
     ports = []
     results = {
-        "time": scan_time,
+        "start_time": start_time,
+        "end_time": end_time,
         "address": address,
         "address_type": address_type,
         "host_names": host_names,
-        "ports": ports
+        "ports": ports,
+        "os_info": os_info
     }
 
     process_host_names(host_names, host)
 
     process_ports(ports, host)
+
+    process_os(os_info, host)
+
+    if hasattr(host, "status"):
+        status = host.status
+        results["status"] = status["state"]
+        results["status_reason"] = status["reason"]
+
+    if hasattr(host, "uptime"):
+        uptime = host.uptime
+        results["uptime"] = uptime["seconds"]
+        results["last_boot"] = uptime["lastboot"]
 
     post_results(topic, f"{task_name}:data", results)
 
@@ -73,18 +91,53 @@ def process_host_results(topic, host):
 
 def process_ports(ports, host):
     for port in host.ports.port:
+        status = port.state
         port_id, protocol = (port['portid'], port['protocol'])
         print(f"Looking at port: {(port_id, protocol)}")
-        ports.append({
+        port_info = {
             "port_id": port_id,
-            "protocol": protocol,
-            "state": port.state["state"],
+            "protocol": protocol
+        }
+        if hasattr(port, "state"):
+            status = port.state
+            port_info["status"] = status["state"]
+            port_info["status_reason"] = status["reason"]
+        process_port_service(port_info, port)
+        process_port_scripts(port_info, port)
+        ports.append(port_info)
+
+
+def process_port_service(port_info, port):
+    if hasattr(port, "service"):
+        port_info.update({
             "service": port.service["name"],
             "product": port.service["product"],
             "version": port.service["version"],
             "extra_info": port.service["extrainfo"],
-            "os_type": port.service["ostype"]
-        })
+            "os_type": port.service["ostype"],
+            })
+        if hasattr(port.service, "cpe"):
+            cpes = []
+            for cpe in port.service.cpe:
+                cpes.append(cpe.cdata)
+            if len(cpes) > 0:
+                port_info["cpes"] = cpes
+
+
+def process_port_scripts(port_info, port):
+    if hasattr(port, "script"):
+        scripts = {}
+        for script in port.script:
+            name = script["id"]
+            # try and dynamically load a module for each script
+            script_processing_module_spec = importlib.util.find_spec(f"results_parser.{name}")
+            if script_processing_module_spec:
+                print(f"Processing plugin for script {name}")
+                module = importlib.util.module_from_spec(script_processing_module_spec)
+                script_processing_module_spec.loader.exec_module(module)
+                script_info = module.process_script(script)
+                if script_info:
+                    port_info.update(script_info)
 
 
 def process_host_names(host_names, host):
@@ -93,6 +146,29 @@ def process_host_names(host_names, host):
             "host_name": host_name["name"],
             "host_name_type": host_name["type"]
         })
+
+
+MAPPED_OS_ATTRS = {f: f.replace("_", "") for f in ["type", "vendor", "os_family", "os_gen", "accuracy"]}
+
+
+def process_os(os_info, host):
+    for os_match in host.os.osmatch:
+        os_details = {
+            "os_name": os_match["name"],
+            "os_accuracy": os_match["accuracy"]
+        }
+        if hasattr(os_match, "osclass"):
+            os_classes = []
+            for os_class in os_match.osclass:
+                class_info = {}
+                for field, retreive_name in MAPPED_OS_ATTRS.items():
+                    class_info[f"os_class_{field}"] = os_class[retreive_name]
+                    if hasattr(os_class, "cpe"):
+                        class_info["os_cpe"] = os_class.cpe.cdata
+                os_classes.append(class_info)
+            if len(os_classes) > 0:
+                os_details["os_classes"] = os_classes
+        os_info.append(os_details)
 
 
 @ssm_parameters(
