f16cfc65bc27e6d8743e701fdb1fad2a371a9521
Matthew Amos <mamos@scottlogic.com>
2019-05-23 15:03:04 +0100

    Added extra visulations

--- infrastructure/elastic_resources/indexes.tf
+++ infrastructure/elastic_resources/indexes.tf
@@ -385,3 +385,66 @@ module "index_pattern_ssl_ciphers_snapshot" {
   object_type  = "index-pattern"
   object_title = "${var.task_name}:ssl_ciphers_snapshot:read*"
 }
+
+module "nmap_index_ssl_cert" {
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/elastic_index"
+
+  // It is sometimes useful for the developers of the project to use a local version of the task
+  // execution project. This enables them to develop the task execution project and the nmap scanner
+  // (or other future tasks), at the same time, without requiring the task execution changes to be
+  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  // devs will have to comment in/out this line as and when they need
+  // source = "../../../securityanalytics-analyticsplatform/infrastructure/elastic_index"
+  app_name = "${var.app_name}"
+
+  aws_region       = "${var.aws_region}"
+  ssm_source_stage = "${var.ssm_source_stage}"
+  index_file       = "${path.module}/indexes/nmap-ssl_cert.index.json"
+  index_name       = "ssl_cert"
+  task_name        = "${var.task_name}"
+}
+
+module "index_pattern_ssl_cert_history" {
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
+
+  // It is sometimes useful for the developers of the project to use a local version of the task
+  // execution project. This enables them to develop the task execution project and the nmap scanner
+  // (or other future tasks), at the same time, without requiring the task execution changes to be
+  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  // devs will have to comment in/out this line as and when they need
+  // source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  app_name = "${var.app_name}"
+
+  aws_region           = "${var.aws_region}"
+  ssm_source_stage     = "${var.ssm_source_stage}"
+  task_name            = "${var.task_name}"
+  object_template      = "${path.module}/indexes/nmap-ssl_cert.pattern.json"
+  object_substitutions = {}
+
+  object_type  = "index-pattern"
+  object_title = "${var.task_name}:ssl_cert_history:read*"
+}
+
+module "index_pattern_ssl_cert_snapshot" {
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
+
+  // It is sometimes useful for the developers of the project to use a local version of the task
+  // execution project. This enables them to develop the task execution project and the nmap scanner
+  // (or other future tasks), at the same time, without requiring the task execution changes to be
+  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  // devs will have to comment in/out this line as and when they need
+  // source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  app_name = "${var.app_name}"
+
+  aws_region           = "${var.aws_region}"
+  ssm_source_stage     = "${var.ssm_source_stage}"
+  task_name            = "${var.task_name}"
+  object_template      = "${path.module}/indexes/nmap-ssl_cert.pattern.json"
+  object_substitutions = {}
+
+  object_type  = "index-pattern"
+  object_title = "${var.task_name}:ssl_cert_snapshot:read*"
+}
---
+++ infrastructure/elastic_resources/visulations_history.tf
@@ -0,0 +1,49 @@
+module "host_status" {
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
+
+  // It is sometimes useful for the developers of the project to use a local version of the task
+  // execution project. This enables them to develop the task execution project and the nmap scanner
+  // (or other future tasks), at the same time, without requiring the task execution changes to be
+  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  // devs will have to comment in/out this line as and when they need
+  //  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  app_name = "${var.app_name}"
+
+  aws_region       = "${var.aws_region}"
+  ssm_source_stage = "${var.ssm_source_stage}"
+  task_name        = "${var.task_name}"
+  object_template  = "${path.module}/visualisations/historical/host_status.vis.json"
+
+  object_substitutions {
+    index = "${module.index_pattern_history.object_id}"
+  }
+
+  object_type  = "visualization"
+  object_title = "Up/Down"
+}
+
+module "host_scanned" {
+  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
+
+  // It is sometimes useful for the developers of the project to use a local version of the task
+  // execution project. This enables them to develop the task execution project and the nmap scanner
+  // (or other future tasks), at the same time, without requiring the task execution changes to be
+  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  // devs will have to comment in/out this line as and when they need
+  //  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  app_name = "${var.app_name}"
+
+  aws_region       = "${var.aws_region}"
+  ssm_source_stage = "${var.ssm_source_stage}"
+  task_name        = "${var.task_name}"
+  object_template  = "${path.module}/visualisations/historical/hosts_scanned.vis.json"
+
+  object_substitutions {
+    index = "${module.index_pattern_history.object_id}"
+  }
+
+  object_type  = "visualization"
+  object_title = "Hosts scanned"
+}
