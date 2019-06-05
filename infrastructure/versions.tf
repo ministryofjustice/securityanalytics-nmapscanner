aedf000edb1d1dfc46ae7066910496565968c476
Matthew Amos <mamos@scottlogic.com>
2019-06-05 18:06:16 +0100

    Fix versions and shared code &c

--- infrastructure/versions.tf
+++ infrastructure/versions.tf
@@ -2,3 +2,19 @@
 terraform {
   required_version = ">= 0.12"
 }
+
+provider "null" {
+  version = "~> 2.1"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}
+
+provider "external" {
+  version = "~> 1.1"
+}
+
+provider "local" {
+  version = "~> 1.2"
+}
