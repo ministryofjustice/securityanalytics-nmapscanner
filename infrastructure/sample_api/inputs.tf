454fc6a857c4a75c30977cbf0cd2f7263b190ca5
Matthew Amos <mamos@scottlogic.com>
2019-07-02 21:44:00 +0100

    Simplifying nmap scan by pushing things into ecs task

---
+++ infrastructure/sample_api/inputs.tf
@@ -0,0 +1,31 @@
+variable "account_id" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "task_name" {
+  type = string
+}
+
+variable "use_xray" {
+  type = string
+}
+
+variable "transient_workspace" {
+  type = string
+}
+
+variable "ssm_source_stage" {
+  type = string
+}
+
+variable "lambda_zip" {
+  type = string
+}
