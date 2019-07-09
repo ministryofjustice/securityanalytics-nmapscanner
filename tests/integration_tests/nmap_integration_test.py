66bd0986d34db2ed5386d8731ee835e774b7ad34
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-07-09 16:35:12 +0100

    Feature/sa 12 integration test (#66)
    Added an nmap integration test

---
+++ tests/integration_tests/nmap_integration_test.py
@@ -0,0 +1,41 @@
+import pytest
+from json import loads
+import re
+from tests.scan_integration_test_utils.scan_integration_tester import ScanIntegrationTester
+
+MESSAGE_ID = re.compile(r"^([a-f0-9]{8}(-[a-f0-9]{4}){3}-[a-f0-9]{12}).*$")
+
+
+@pytest.mark.integration
+@pytest.mark.asyncio
+async def test_integration():
+    timeout = 240
+
+    class NmapIntegrationTester(ScanIntegrationTester):
+        def __init__(self, timeout_seconds=120):
+            super().__init__(timeout_seconds)
+            self.request_msg_id = None
+
+        async def send_request(self):
+            resp = await self.sqs_client.send_message(
+               QueueUrl=self.sqs_input_queue_url,
+               # TODO relying on an external resource like this is error prone and unreliable,
+               # we should setup a host to scan as part of the test setup instead
+               MessageBody="scanme.nmap.org"
+            )
+            self.request_msg_id = resp["MessageId"]
+            print(f"Made request {self.request_msg_id}")
+
+        async def handle_results(self, body):
+            result = loads(loads(body)["Message"])
+            scan_id = result["scan_id"]
+
+            # TODO need a proper tracing id, this pulling out of the scan of the original
+            # input queue message id is not good enough
+            original_msg_id_from_scan_id = re.match(MESSAGE_ID, scan_id)[1]
+            if original_msg_id_from_scan_id == self.request_msg_id:
+                assert True
+                print(f"Have received results for initial request with message id {self.request_msg_id}", flush=True)
+                await self.cancel_polling()
+
+    await NmapIntegrationTester(timeout).run_test()
